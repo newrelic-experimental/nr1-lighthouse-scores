e163233969f4f1695f9d5633f72e5d6eaf6b8d1b
rdouglas <ruairidouglas@gmail.com>
2022-06-07 13:02:56 +0100

    fix: account id not populating in build script

--- nerdlets/build-script/components/BuildScriptModal.js
+++ nerdlets/build-script/components/BuildScriptModal.js
@@ -18,9 +18,10 @@ import {
   Form,
   CardSection,
   AccountPicker,
+  NerdletStateContext,
 } from "nr1";
 import { legacyScoreScript, scoreScript } from "../../../src/utils/constants";
-import {isUrlSafe} from "../../../utils/helpers";
+import { isUrlSafe } from "../../../utils/helpers";
 import Editor from "react-simple-code-editor";
 import { highlight, languages } from "prismjs/components/prism-core";
 import "prismjs/components/prism-clike";
@@ -113,7 +114,7 @@ export default class BuildScriptModal extends React.Component {
     });
   };
 
-  _buildScript = async () => {
+  _buildScript = async (stateAccountId) => {
     this.setState({ scriptLoading: true });
     const {
       selectedAudits,
@@ -122,15 +123,14 @@ export default class BuildScriptModal extends React.Component {
       nrLicenseKey,
       pageSpeedApiKey,
       nodeVersion,
+      accountId,
     } = this.state;
-    const { accountId } = this.props;
     let isValid =
       Object.values({
         url,
         nrLicenseKey,
-        accountId,
+        stateAccountId,
         pageSpeedApiKey,
-        accountId,
       }).filter((v) => v.length === 0).length === 0;
 
     if (url && !isUrlSafe(url)) {
@@ -159,7 +159,7 @@ const PAGE_SPEED_KEY = ${
         ? pageSpeedApiKey
         : `'${pageSpeedApiKey}'`
     };
-const ACCOUNT_ID = '${accountId}';
+const ACCOUNT_ID = '${accountId || stateAccountId}';
 const EVENT_URL = '${event_url}';
       ${nodeVersion === "node16" ? scoreScript : legacyScoreScript}
     `;
@@ -181,193 +181,214 @@ const EVENT_URL = '${event_url}';
       nodeVersion,
       showScript,
       scriptLoading,
+      accountId,
     } = this.state;
-    const { accountId } = this.props;
 
     return (
-      <Card>
-        <CardBody>
-          <HeadingText
-            spacingType={[HeadingText.SPACING_TYPE.LARGE]}
-            type={HeadingText.TYPE.HEADING_2}
-          >
-            Build your script
-          </HeadingText>
-          <CardSection />
-          <Card>
-            <CardBody>
-              <Grid>
-                <GridItem columnSpan={4}>
-                  <Form>
-                    <AccountPicker
-                      value={accountId}
-                      onChange={this._setAccountId}
-                      label="Account"
-                      invalid={
-                        !isValid && !accountId ? "Please select an account" : ""
-                      }
-                      required
-                    />
-                    <TextField
-                      placeholder="https://www.example.com"
-                      value={url}
-                      onChange={this._setUrl}
-                      label="URL"
-                      style={{ width: "100%" }}
-                      invalid={
-                        (!isValid && !url) ||
-                        (url && !isUrlSafe(url))
-                          ? "Please enter a valid URL"
-                          : ""
-                      }
-                      description={urlStatus && `Status: ${urlStatus}`}
-                      required
-                      autoFocus
-                    />
-                    <Select
-                      description="Description value"
-                      label="Select Device"
-                      info="Please ensure you use the same setting when creating your monitor"
-                      value={strategy}
-                      onChange={this._onSelectStrategy}
-                    >
-                      <SelectItem value="desktop">Desktop</SelectItem>
-                      <SelectItem value="mobile">Mobile</SelectItem>
-                    </Select>
-                    <Select
-                      description="Description value"
-                      label="Select Node Version"
-                      info="Legacy Node monitors use the request module. New monitors should use get"
-                      value={nodeVersion}
-                      onChange={this._onSelectNodeVersion}
-                    >
-                      <SelectItem value="node16">Node 16.0.0</SelectItem>
-                      <SelectItem value="node10">Node 10 (Legacy)</SelectItem>
-                    </Select>
-                    <CheckboxGroup
-                      label="Select Audits"
-                      value={selectedAudits}
-                      onChange={this._onSelectAudits}
-                      required
-                    >
-                      <Checkbox
-                        checked
-                        disabled
-                        label="Performance"
-                        value="performance"
-                      />
-                      <Checkbox label="Accessibility" value="accessibility" />
-                      <Checkbox label="Best Practices" value="best-practices" />
-                      <Checkbox label="PWA" value="pwa" />
-                      <Checkbox label="SEO" value="seo" />
-                    </CheckboxGroup>
-                    <Link
-                      onClick={() =>
-                        navigation.openLauncher({
-                          id: "api-keys-ui.home",
-                        })
-                      }
-                      target="_blank"
-                    >
-                      Get License Key
-                    </Link>
-                    <TextField
-                      label="New Relic License Key"
-                      placeholder="XXXNRAL or $secure.LICENSE_KEY"
-                      value={nrLicenseKey}
-                      invalid={
-                        !isValid &&
-                        !nrLicenseKey &&
-                        "Please enter a New Relic License Key"
-                      }
-                      info="This is for the account you want the Events to report to"
-                      style={{ width: "100%" }}
-                      onChange={this._setNrLicenseKey}
-                      required
-                    />
-                    <Link to="https://developers.google.com/speed/docs/insights/v5/get-started#APIKey">
-                      Generate PageSpeed API Key
-                    </Link>
-                    <TextField
-                      label="PageSpeed API Key"
-                      placeholder="XXX or $secure.PAGESPEED_API_KEY"
-                      value={pageSpeedApiKey}
-                      invalid={
-                        !isValid &&
-                        !pageSpeedApiKey &&
-                        "Please enter a PageSpeed API key"
-                      }
-                      style={{ width: "100%" }}
-                      onChange={this._setPageSpeedApiKey}
-                      required
-                    />
-                  </Form>
-                </GridItem>
-                <GridItem columnSpan={1}>
-                  <Card>
-                    <CardBody>
-                      <Button
-                        iconType={
-                          Button.ICON_TYPE
-                            .INTERFACE__CHEVRON__CHEVRON_RIGHT__WEIGHT_BOLD__SIZE_8
-                        }
-                        type={Button.TYPE.PRIMARY}
-                        onClick={this._buildScript}
-                      >
-                        Build
-                      </Button>
-                    </CardBody>
-                  </Card>
-                </GridItem>
-                <GridItem columnSpan={7}>
-                  <TextField
-                    label="Suggested monitor name"
-                    placeholder="XXX or $secure.PAGESPEED_API_KEY"
-                    value={monitorName}
-                    style={{ width: "100%" }}
-                    readOnly
-                  />
-                  {scriptLoading && <Spinner type={Spinner.TYPE.DOT} />}
-                  {showScript && (
-                    <Card>
-                      <CardBody>
-                        <HeadingText
-                          spacingType={[HeadingText.SPACING_TYPE.LARGE]}
-                          type={HeadingText.TYPE.HEADING_4}
-                        >
-                          Scripted API monitor
-                        </HeadingText>
-                        <BlockText
-                          spacingType={[HeadingText.SPACING_TYPE.LARGE]}
-                        >
-                          Create a Synthetics scripted API monitor with the
-                          settings you've provided to ensure we can perform the
-                          correct data correlation
-                        </BlockText>
-                        <Editor
-                          className="language-javascript"
-                          value={code}
-                          onValueChange={(code) => this.setState({ code })}
-                          highlight={(code) => highlight(code, languages.js)}
-                          padding={10}
-                          style={{
-                            backgroundColor: "#f5f5f5",
-                            maxHeight: "450px",
-                            overflow: "auto",
-                            fontFamily:
-                              "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
-                            fontSize: 12,
-                          }}
+      <NerdletStateContext.Consumer>
+        {(nerdletState) => {
+          const { stateAccountId } = nerdletState;
+
+          return (
+            <Card>
+              <CardBody>
+                <HeadingText
+                  spacingType={[HeadingText.SPACING_TYPE.LARGE]}
+                  type={HeadingText.TYPE.HEADING_2}
+                >
+                  Build your script
+                </HeadingText>
+                <CardSection />
+                <Card>
+                  <CardBody>
+                    <Grid>
+                      <GridItem columnSpan={4}>
+                        <Form>
+                          <AccountPicker
+                            value={accountId || stateAccountId}
+                            onChange={this._setAccountId}
+                            label="Account"
+                            invalid={
+                              !isValid && !stateAccountId && !accountId
+                                ? "Please select an account"
+                                : ""
+                            }
+                            required
+                          />
+                          <TextField
+                            placeholder="https://www.example.com"
+                            value={url}
+                            onChange={this._setUrl}
+                            label="URL"
+                            style={{ width: "100%" }}
+                            invalid={
+                              (!isValid && !url) || (url && !isUrlSafe(url))
+                                ? "Please enter a valid URL"
+                                : ""
+                            }
+                            description={urlStatus && `Status: ${urlStatus}`}
+                            required
+                            autoFocus
+                          />
+                          <Select
+                            description="Description value"
+                            label="Select Device"
+                            info="Please ensure you use the same setting when creating your monitor"
+                            value={strategy}
+                            onChange={this._onSelectStrategy}
+                          >
+                            <SelectItem value="desktop">Desktop</SelectItem>
+                            <SelectItem value="mobile">Mobile</SelectItem>
+                          </Select>
+                          <Select
+                            description="Description value"
+                            label="Select Node Version"
+                            info="Legacy Node monitors use the request module. New monitors should use get"
+                            value={nodeVersion}
+                            onChange={this._onSelectNodeVersion}
+                          >
+                            <SelectItem value="node16">Node 16.0.0</SelectItem>
+                            <SelectItem value="node10">
+                              Node 10 (Legacy)
+                            </SelectItem>
+                          </Select>
+                          <CheckboxGroup
+                            label="Select Audits"
+                            value={selectedAudits}
+                            onChange={this._onSelectAudits}
+                            required
+                          >
+                            <Checkbox
+                              checked
+                              disabled
+                              label="Performance"
+                              value="performance"
+                            />
+                            <Checkbox
+                              label="Accessibility"
+                              value="accessibility"
+                            />
+                            <Checkbox
+                              label="Best Practices"
+                              value="best-practices"
+                            />
+                            <Checkbox label="PWA" value="pwa" />
+                            <Checkbox label="SEO" value="seo" />
+                          </CheckboxGroup>
+                          <Link
+                            onClick={() =>
+                              navigation.openLauncher({
+                                id: "api-keys-ui.home",
+                              })
+                            }
+                            target="_blank"
+                          >
+                            Get License Key
+                          </Link>
+                          <TextField
+                            label="New Relic License Key"
+                            placeholder="XXXNRAL or $secure.LICENSE_KEY"
+                            value={nrLicenseKey}
+                            invalid={
+                              !isValid &&
+                              !nrLicenseKey &&
+                              "Please enter a New Relic License Key"
+                            }
+                            info="This is for the account you want the Events to report to"
+                            style={{ width: "100%" }}
+                            onChange={this._setNrLicenseKey}
+                            required
+                          />
+                          <Link to="https://developers.google.com/speed/docs/insights/v5/get-started#APIKey">
+                            Generate PageSpeed API Key
+                          </Link>
+                          <TextField
+                            label="PageSpeed API Key"
+                            placeholder="XXX or $secure.PAGESPEED_API_KEY"
+                            value={pageSpeedApiKey}
+                            invalid={
+                              !isValid &&
+                              !pageSpeedApiKey &&
+                              "Please enter a PageSpeed API key"
+                            }
+                            style={{ width: "100%" }}
+                            onChange={this._setPageSpeedApiKey}
+                            required
+                          />
+                        </Form>
+                      </GridItem>
+                      <GridItem columnSpan={1}>
+                        <Card>
+                          <CardBody>
+                            <Button
+                              iconType={
+                                Button.ICON_TYPE
+                                  .INTERFACE__CHEVRON__CHEVRON_RIGHT__WEIGHT_BOLD__SIZE_8
+                              }
+                              type={Button.TYPE.PRIMARY}
+                              onClick={() => this._buildScript(stateAccountId)}
+                            >
+                              Build
+                            </Button>
+                          </CardBody>
+                        </Card>
+                      </GridItem>
+                      <GridItem columnSpan={7}>
+                        <TextField
+                          label="Suggested monitor name"
+                          placeholder="XXX or $secure.PAGESPEED_API_KEY"
+                          value={monitorName}
+                          style={{ width: "100%" }}
+                          readOnly
                         />
-                      </CardBody>
-                    </Card>
-                  )}
-                </GridItem>
-              </Grid>
-            </CardBody>
-          </Card>
-        </CardBody>
-      </Card>
+                        {scriptLoading && <Spinner type={Spinner.TYPE.DOT} />}
+                        {showScript && (
+                          <Card>
+                            <CardBody>
+                              <HeadingText
+                                spacingType={[HeadingText.SPACING_TYPE.LARGE]}
+                                type={HeadingText.TYPE.HEADING_4}
+                              >
+                                Scripted API monitor
+                              </HeadingText>
+                              <BlockText
+                                spacingType={[HeadingText.SPACING_TYPE.LARGE]}
+                              >
+                                Create a Synthetics scripted API monitor with
+                                the settings you've provided to ensure we can
+                                perform the correct data correlation
+                              </BlockText>
+                              <Editor
+                                className="language-javascript"
+                                value={code}
+                                onValueChange={(code) =>
+                                  this.setState({ code })
+                                }
+                                highlight={(code) =>
+                                  highlight(code, languages.js)
+                                }
+                                padding={10}
+                                style={{
+                                  backgroundColor: "#f5f5f5",
+                                  maxHeight: "450px",
+                                  overflow: "auto",
+                                  fontFamily:
+                                    "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+                                  fontSize: 12,
+                                }}
+                              />
+                            </CardBody>
+                          </Card>
+                        )}
+                      </GridItem>
+                    </Grid>
+                  </CardBody>
+                </Card>
+              </CardBody>
+            </Card>
+          );
+        }}
+      </NerdletStateContext.Consumer>
     );
   }
 }
